cc1cd5365a51cdf41693d7bb832fd1a6d64742cd
BorEaL <16733@supinfo.com>
2017-01-29 18:30:46 -0500

    test

---
+++ src/concordia/ShowButtonDemo.java
@@ -0,0 +1,19 @@
+package concordia;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yufei
+ */
+public class ShowButtonDemo
+{
+public static void main(String[] args)
+{
+ButtonDemo gui = new ButtonDemo();
+gui.setVisible(true);
+}
+}
