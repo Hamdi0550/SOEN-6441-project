ebab4b37c6e07fd541fe0a41a42bd38dd1becfef
BorEaL <16733@supinfo.com>
2017-02-22 20:12:11 -0500

    map and Cell model
    modify the map model and add cell model

---
+++ src/model/Cell.java
@@ -0,0 +1,18 @@
+package model;
+/**
+ * Cells class which include content on the Map
+ * @author Bo
+ * @Feb 22,2017
+ */
+public class Cell {
+	private Object content;
+
+	public Object getContent() {
+		return content;
+	}
+
+	public void setContent(Object content) {
+		this.content = content;
+	}
+	
+}
