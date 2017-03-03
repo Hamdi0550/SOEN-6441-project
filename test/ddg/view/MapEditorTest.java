bbb56e7bf916344a07fc3b62cd48b77e188b445e
BorEaL <167233@supinfo.com>
2017-03-02 20:02:32 -0500

    add Validation only one entry door

--- test/ddg/view/MapEditorTest.java
+++ test/ddg/view/MapEditorTest.java
@@ -1,5 +1,9 @@
 package ddg.view;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 import ddg.Config;
 import ddg.model.MapEditorModel;
 import ddg.utils.Utils;
@@ -21,10 +25,29 @@ public class MapEditorTest extends TestCase {
 
     public void setUp() throws Exception {
         super.setUp();
-        mapModel = new MapEditorModel();
-        String g = Utils.readFile(Config.MAP_FILE);
-        mapModel = Utils.fromJson(g, MapEditorModel.class);
-        map = mapModel.getMapByIndex(1);
+        try
+	      {
+	         FileInputStream fileIn = new FileInputStream(Config.MAP_FILE);
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         mapModel = (MapEditorModel) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      }catch(IOException i)
+	      {
+	         i.printStackTrace();
+	      }catch(ClassNotFoundException c)
+	      {
+	         c.printStackTrace();
+	      }catch(Exception e){
+	    	  e.printStackTrace();
+	      }
+        
+        if(mapModel==null){
+        	mapModel = new MapEditorModel();
+        	mapModel.add(new Map()));
+        }
+        
+        map = mapModel.getMapByIndex(0);
         validationTool = new ValidationTool(map);
 
     }
