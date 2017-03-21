c256b41f67f4091f912f0baca55cd14b1269979f
hamzah <hamzah.r.1@hotmail.com>
2017-03-21 16:43:27 -0400

    Adding the test methods

---
+++ test/ddg/view/gaming/GameInitDialogTest.java
@@ -0,0 +1,19 @@
+package ddg.view.gaming;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GameInitDialogTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
