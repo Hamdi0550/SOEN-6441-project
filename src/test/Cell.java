72427dfba82ef6190b7a927c003906823c4bbc6c
Zhen Du <jenkin.du@gmail.com>
2017-03-20 23:19:42 -0400

    test

---
+++ src/test/Cell.java
@@ -0,0 +1,33 @@
+package test;
+/**
+ * Cells class which include content on the Map
+ * @author Bo
+ * @Feb 22,2017
+ */
+public class Cell {
+	private Object content;
+	private boolean isfriendly;
+
+	public Cell(Object content){
+		this.content = content;
+		this.isfriendly = true;
+	}
+	
+	public Cell(Object content, boolean isfriendly){
+		this.content = content;
+		this.isfriendly = isfriendly;
+	}
+	
+	public Object getContent() {
+		return content;
+	}
+
+	public void setContent(Object content) {
+		this.content = content;
+	}
+	
+	public boolean getIsfriendly(){
+		return isfriendly;
+	}
+	
+}
