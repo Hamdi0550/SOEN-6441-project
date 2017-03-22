2412e3dbd093cadcf4b60a4134ead31252520c85
QinYi93 <y_qin201@encs.concordia.ca>
2017-03-22 19:51:07 -0400

    Update Junit Test case suite

--- test/ddg/view/AllTests.java
+++ test/ddg/view/AllTests.java
@@ -4,8 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import ddg.utils.builder.FighterExplorerTest;
+import ddg.view.gaming.GameInitDialogTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({ CampaignEditorTest.class, ItemEditorTest.class, MapEditorTest.class, CharactorEditorTest.class })
+@SuiteClasses({ CampaignEditorTest.class, ItemEditorTest.class, MapEditorTest.class, CharactorEditorTest.class, MapPanelInGameTest.class, FighterExplorerTest.class, GameInitDialogTest.class})
 public class AllTests {
 
 }
