7debe503c06ca130146f60a9b8a61d5e79cb5a38
hamzah <hamzah.r.1@hotmail.com>
2017-03-22 22:18:43 -0400

    Add a test case for backpackTrade

---
+++ test/ddg/view/BackpackTradeTest.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package ddg.view;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Iterator;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ddg.Config;
+import ddg.item.entity.BaseItem;
+import ddg.model.Fighter;
+import ddg.model.FighterModel;
+import ddg.utils.Utils;
+
+/**
+ * This class
+ * 
+ * @author Hamzah Hamdi
+ * @date Mar 22, 2017
+ */
+public class BackpackTradeTest {
+
+	private static FighterModel fighterModel;
+	private Fighter player;
+	private Fighter npc;
+	/**
+	 * This method
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		String g = Utils.readFile(Config.CHARACTOR_FILE);
+		fighterModel = Utils.fromJson(g, FighterModel.class);
+	}
+
+	/**
+	 * This method
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		Iterator<Fighter> iterator = fighterModel.getFighters().values().iterator();
+		while (iterator.hasNext()) {
+			Fighter next = iterator.next();
+			if (next.getBackpack().size() > 0) {
+				if (player == null) {
+					player = next;
+				} else if (npc == null) {
+					npc = next;
+				}
+			}
+		}
+	}
+
+	@Test
+	public void testExitsPlayer() {
+		assertNotNull(player);
+		assertNotNull(npc);
+	}
+}
