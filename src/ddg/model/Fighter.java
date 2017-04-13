50deca54d405974c86761b834e64f05113cfb2a2
Fei Yu <chrisfyu@hotmail.com>
2017-04-13 13:28:39 -0400

    update fighter

--- src/ddg/model/Fighter.java
+++ src/ddg/model/Fighter.java
@@ -456,7 +456,7 @@ public class Fighter extends Observable implements IOwner, Cloneable, Serializab
 				System.out.println("this weanpon is ranged weapon attackBonus: "+attackBonus);
 			}
 		} else {
-			attackBonus = level;
+			attackBonus = getStrModifier();
 			MapPanelInGame.printLog(" attack bonus: level " + attackBonus);
 		}
 		return attackBonus;
