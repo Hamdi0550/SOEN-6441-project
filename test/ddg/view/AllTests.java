0fadd771eafefbea6aa59704e19c5d036ed1425b
Zhen Du <jenkin.du@gmail.com>
2017-04-13 13:16:36 -0400

    commit data

--- test/ddg/view/AllTests.java
+++ test/ddg/view/AllTests.java
@@ -5,12 +5,14 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import ddg.builder.FighterExplorerTest;
+import ddg.model.FighterTest;
+import ddg.model.item.MagicWeaponItemTest;
 import ddg.utils.DiceTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CampaignEditorTest.class, ItemEditorTest.class, MapEditorTest.class, CharactorEditorTest.class,
 		MapPanelInGameTest.class, FighterExplorerTest.class, GameInitDialogTest.class, BackpackTradeTest.class,
-		ChestTest.class, DiceTest.class})
+		ChestTest.class, DiceTest.class, MagicWeaponItemTest.class, FighterTest.class})
 public class AllTests {
 
 }
