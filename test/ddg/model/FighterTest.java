d8159f27f389eedd28c0fcd7fc8e82d0627fceff
Zhen Du <jenkin.du@gmail.com>
2017-04-13 14:38:51 -0400

    add test case

--- test/ddg/model/FighterTest.java
+++ test/ddg/model/FighterTest.java
@@ -1,19 +1,29 @@
 package ddg.model;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
+
+import java.awt.Point;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import ddg.Config;
+import ddg.model.entity.BaseCampaign;
+import ddg.strategy.FriendlyStrategy;
 import ddg.utils.Dice;
 import ddg.utils.Utils;
 
 public class FighterTest {
 
-	static Fighter[] player = new Fighter[3];
+	static Fighter[] player = new Fighter[4];
 
+	/**
+	 * 
+	 * This method setUpBeforeClass
+	 * 
+	 * @throws Exception
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		FighterModel fighterModel = Utils.readObject(Config.CHARACTER_FILE, FighterModel.class);
@@ -22,44 +32,94 @@ public class FighterTest {
 		player[2] = fighterModel.getFighters().get("Tank3");
 	}
 
+	/**
+	 * 
+	 * This method is testAttackRollRanged
+	 *
+	 */
 	@Test
 	public void testAttackRollMelee() {
 		assertEquals(player[1].getAttackBonus(), player[1].getStrModifier());
 	}
 
+	/**
+	 * 
+	 * This method
+	 *
+	 */
 	@Test
 	public void testAttackRollRanged() {
 		assertEquals(player[2].getAttackBonus(), player[2].getDexModifier());
 	}
-	
+
+	/**
+	 * 
+	 * This method is testAttackRollNoWeapon
+	 *
+	 */
 	@Test
 	public void testAttackRollNoWeapon() {
 		assertEquals(player[0].getAttackBonus(), player[0].getLevel());
 	}
-	
+
+	/**
+	 * 
+	 * This method is testAttackHits
+	 *
+	 */
 	@Test
 	public void testAttackHits() {
 		int attackRoll = Dice.d20Roll() + player[0].getAttackBonus();
 		int origin = player[1].getHitPoints();
-		if(attackRoll >= player[1].getArmorClass()){
+		if (attackRoll >= player[1].getArmorClass()) {
 			player[1].beAttacked(player[0].getDamageBonus());
 			assertTrue(origin >= player[1].getHitPoints());
 		} else {
 			assertEquals(origin, player[1].getHitPoints());
 		}
-		
+
 	}
-	
+
+	/**
+	 * 
+	 * This method is testAttackDamage
+	 *
+	 */
 	@Test
 	public void testAttackDamage() {
 		int attackRoll = Dice.d20Roll() + player[0].getAttackBonus();
 		int origin = player[1].getHitPoints();
-		if(attackRoll >= player[1].getArmorClass()){
+		if (attackRoll >= player[1].getArmorClass()) {
 			player[1].beAttacked(player[0].getDamageBonus());
-			assertTrue(origin-player[1].getHitPoints() == player[0].getDamageBonus());
+			assertTrue(origin - player[1].getHitPoints() == player[0].getDamageBonus());
 		} else {
 			assertEquals(origin, player[1].getHitPoints());
 		}
-		
+
+	}
+
+	/**
+	 * 
+	 * This method is testCharacterStrategy
+	 *
+	 */
+	@Test
+	public void testCharacterStrategy() {
+		FighterModel fighterModel = Utils.readObject(Config.CHARACTER_FILE, FighterModel.class);
+		player[3] = fighterModel.getFighters().get("Tank4");
+
+		String g = Utils.readFile(Config.CAMPAIGN_FILE);
+		CampaignEditorModel campaignModel = Utils.fromJson(g, CampaignEditorModel.class);
+		BaseCampaign campaign = campaignModel.getItemByIndex(0);
+
+		Game game = new Game(new GameModel(player[3], campaign), null);
+		Point original = new Point(game.getXofplayer(), game.getYofplayer());
+
+		player[3].setStrategy(new FriendlyStrategy(game, player[3]));
+		player[3].turn();
+
+		Point newPoint = new Point(game.getXofplayer(), game.getYofplayer());
+
+		assertNotEquals(original, newPoint);
 	}
 }
--- test/ddg/model/item/MagicWeaponItemTest.java
+++ test/ddg/model/item/MagicWeaponItemTest.java
@@ -17,6 +17,12 @@ public class MagicWeaponItemTest {
 	Fighter player;
 	Fighter npc;
 	
+	/**
+	 * 
+	 * This method setUp before running
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		FighterModel fighterModel = Utils.readObject(Config.CHARACTER_FILE, FighterModel.class);
@@ -24,39 +30,82 @@ public class MagicWeaponItemTest {
 		npc = fighterModel.getFighters().get("Tank4");
 	}
 
+	/**
+	 * 
+	 * This method is testFreezing
+	 *
+	 */
 	@Test
 	public void testFreezing() {
 		Item i = player.getWearItemByName(Item.WEAPON);
-		if (player.getWearItemByName(Item.WEAPON) instanceof MagicWeaponItem){
+		if (i instanceof MagicWeaponItem){
 			((MagicWeaponItem)i).attack(npc);
+			assertTrue(npc.getMagicStrategy()[0] != null);
+		} else {
+			assertFalse(npc.getMagicStrategy()[0] != null);			
 		}
-		assertTrue(npc.getMagicStrategy()[0] != null);
 	}
 
+	/**
+	 * 
+	 * This method is testBuring
+	 *
+	 */
 	@Test
 	public void testBuring() {
 		Item i = player.getWearItemByName(Item.WEAPON);
-		if (player.getWearItemByName(Item.WEAPON) instanceof MagicWeaponItem){
+		if (i instanceof MagicWeaponItem){
 			((MagicWeaponItem)i).attack(npc);
+			assertFalse(npc.getMagicStrategy()[1] == null);
+		} else {
+			assertTrue(npc.getMagicStrategy()[1] == null);
 		}
-		assertFalse(npc.getMagicStrategy()[1] == null);
 	}
 	
+	/**
+	 * 
+	 * This method is testFrightening
+	 *
+	 */
 	@Test
 	public void testFrightening() {
 		Item i = player.getWearItemByName(Item.WEAPON);
-		if (player.getWearItemByName(Item.WEAPON) instanceof MagicWeaponItem){
+		if (i instanceof MagicWeaponItem){
 			((MagicWeaponItem)i).attack(npc);
+			assertTrue(npc.getMagicStrategy()[2] instanceof FrighteningStrategy);
+		} else {
+			assertFalse(npc.getMagicStrategy()[2] instanceof FrighteningStrategy);
 		}
-		assertTrue(npc.getMagicStrategy()[2] instanceof FrighteningStrategy);
 	}
 	
+	/**
+	 * 
+	 * This method is testPacifying
+	 *
+	 */
 	@Test
 	public void testPacifying() {
 		Item i = player.getWearItemByName(Item.WEAPON);
-		if (player.getWearItemByName(Item.WEAPON) instanceof MagicWeaponItem){
+		if (i instanceof MagicWeaponItem){
 			((MagicWeaponItem)i).attack(npc);
+			assertTrue(npc.getBehaviorStrategy() instanceof FriendlyStrategy);
+		} else {
+			assertFalse(npc.getBehaviorStrategy() instanceof FriendlyStrategy);
 		}
-		assertTrue(npc.getBehaviorStrategy() instanceof FriendlyStrategy);
+	}
+	
+	/**
+	 * 
+	 * This method is testWeaponRang
+	 *
+	 */
+	@Test
+	public void testWeaponRang() {
+		Item i = player.getWearItemByName(Item.WEAPON);
+		if (((MagicWeaponItem)i).getWeaponType().equals(WeaponItem.WEAPON_MELEE)){
+			assertTrue(((MagicWeaponItem)i).getRange() == 1);
+		} else {
+			assertTrue(((MagicWeaponItem)i).getRange() == 3);
+		}
 	}
 }
